aeb37f9388d2897d065146952d63c21a23d419d8
David Anderson <davea@berkeley.edu>
2024-03-09 22:21:58 +0000

    implement parse_nmvts_section(), and add a test for it

--- cmi/parse_test.php
+++ cmi/parse_test.php
@@ -10,6 +10,44 @@ function test_movement_lines() {
     }
 }
 
-test_movement_lines();
+function do_nmvts_sections($s) {
+    echo "---------------\n";
+    echo "input:\n$s\n";
+    $x = parse_nmvts_sections($s);
+    if (!$x) {
+        echo "output: none\n";
+        return;
+    }
+    echo "output:\n";
+    echo "title: $x->title\n";
+    $n = count($x->sections);
+    echo "$n sections:\n";
+    foreach ($x->sections as $y) {
+        print_r($y);
+    }
+}
+
+// file has DB output format
+function test_nmvts_sections() {
+    $x = '';
+    $lines = file('nmvts_sections.txt');
+    foreach ($lines as $line) {
+        if (strpos($line, '******') !== false) {
+            if ($x) {
+                do_nmvts_sections($x);
+                $x = '';
+            }
+        } else {
+            $x .= $line;
+        }
+    }
+    if ($x) {
+        do_nmvts_sections($x);
+    }
+}
+
+//test_movement_lines();
+
+test_nmvts_sections();
 
 ?>
